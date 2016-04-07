71c5806d0b8cf7988128b87905d419a0ea13a775
Benjamin Jacobs <contact@ttocsneb.com>
2016-04-06 23:20:06 -0600

    Add control functionality to flight controller

--- sim/core/src/com/ttocsneb/quadsim/Main.java
+++ sim/core/src/com/ttocsneb/quadsim/Main.java
@@ -3,8 +3,13 @@ package com.ttocsneb.quadsim;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -12,11 +17,13 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.gushikustudios.rube.RubeScene;
 import com.gushikustudios.rube.loader.RubeSceneLoader;
 
-public class Main extends ApplicationAdapter {
+public class Main extends ApplicationAdapter implements InputProcessor {
+	private static final float MaxForce = 50;
+	private static final int ppm = 50;
 	RubeScene scene;
 
 	Box2DDebugRenderer renderer;
-
+	ShapeRenderer shape;
 	OrthographicCamera cam;
 
 	Body quad;
@@ -26,46 +33,197 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void create() {
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
 		
+
+		shape = new ShapeRenderer();
+		renderer = new Box2DDebugRenderer();
+		cam = new OrthographicCamera(Gdx.graphics.getWidth() / ppm,
+				Gdx.graphics.getHeight() / ppm);
+		// cam.zoom = 10f;
+
+		ai = new QuadAI();
+
+		lastVel = new Vector2();
+		
+		Gdx.input.setInputProcessor(this);
+		
+		init();
+
+	}
+	
+	private void init() {
+		if(scene != null) scene.getWorld().dispose();
 		RubeSceneLoader loader = new RubeSceneLoader();
 		scene = loader.loadScene(Gdx.files.internal("world.json"));
 
 		quad = scene.getNamed(Body.class, "Drone").first();
-
-		renderer = new Box2DDebugRenderer();// new Box2DDebugRenderer(true,
-											// false, false, true, true, true);
-		cam = new OrthographicCamera(5, 5);
-
-		ai = new QuadAI();
 		
-		lastVel = new Vector2();
-
+		
 	}
 
 	Vector2 lastVel;
 
 	@Override
 	public void render() {
+		// Clear the screen to black
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		Vector2 vel = new Vector2(quad.getLinearVelocity());
-		vel.sub(lastVel);
-		vel.x *= scene.stepsPerSecond;
-		vel.y *= scene.stepsPerSecond;
-		vel.y -= scene.getWorld().getGravity().y;
+		// move the camera to the quad.
+		cam.position.set(quad.getPosition().x, quad.getPosition().y, 0);
+		cam.update();
 
-		float totacc = (float) Math.sqrt(vel.x * vel.x + vel.y * vel.y);
+		// Convert linear acceleration to an output similar to that an
+		// accelerometer would give.
+		// First convert the velocity to acceleration.
+		Vector2 acc = new Vector2(quad.getLinearVelocity());
+		acc.sub(lastVel);
+		acc.x *= scene.stepsPerSecond;
+		acc.y *= scene.stepsPerSecond;
+		// add gravity to the y axis (we have not yet accounted for
+		// orientation).
+		acc.y -= scene.getWorld().getGravity().y;
+		// Get the total acceleration in both axis.
+		float totacc = (float) Math.sqrt(acc.x * acc.x + acc.y * acc.y);
+		// calculate the acceleration in the plane of the quad.
+		acc.x = Math.min(
+				8,
+				Math.max(-8, (MathUtils.sin(quad.getAngle()) * totacc)
+						/ -scene.getWorld().getGravity().y));
+		acc.y = Math.min(
+				8,
+				Math.max(-8, (MathUtils.cos(quad.getAngle()) * totacc)
+						/ -scene.getWorld().getGravity().y));
 
-		vel.x = Math.min(8, Math.max(-8, (MathUtils.sin(quad.getAngle()) * totacc)/-scene.getWorld().getGravity().y));
-		vel.y = Math.min(8, Math.max(-8, (MathUtils.cos(quad.getAngle()) * totacc)/-scene.getWorld().getGravity().y));
-
-		ai.update(quad.getAngularVelocity(), vel);
+		// Set the last velocity
 		lastVel = new Vector2(quad.getLinearVelocity());
+
+		// Update the ai with found acceleration, and angular velocity.
+		Vector2 forces = ai.update(quad.getAngularVelocity(), acc);
+		// Convert the forces value into actual forces using the MaxForce Final.
+		forces.x *= MaxForce / 2000f;
+		forces.y *= MaxForce / 2000f;
+		forces.x = MathUtils.clamp(forces.x, 0, MaxForce / 2f);
+		forces.y = MathUtils.clamp(forces.y, 0, MaxForce / 2f);
+
+		// Set the position of the forces to variables (as it will be used
+		// multiple times).
+		Vector2 posleft = new Vector2(quad.getPosition()).add(0.1724f
+				* MathUtils.cos(quad.getAngle()+2.84489f),
+				0.1724f * MathUtils.sin(quad.getAngle()+2.84489f));
+		Vector2 posright = new Vector2(quad.getPosition()).add(
+				0.1724f * MathUtils.cos(quad.getAngle()+0.296706f),
+				0.1724f * MathUtils.sin(quad.getAngle()+0.296706f));
+
+		// apply the left/right forces to the corners of the quad.
+		quad.applyForce(
+				new Vector2(forces.x
+						* MathUtils.cos(0.5f * MathUtils.PI + quad.getAngle()),
+						forces.x
+								* MathUtils.sin(0.5f * MathUtils.PI
+										+ quad.getAngle())), posleft, true);
+		quad.applyForce(
+				new Vector2(forces.y
+						* MathUtils.cos(0.5f * MathUtils.PI + quad.getAngle()),
+						forces.y
+								* MathUtils.sin(0.5f * MathUtils.PI
+										+ quad.getAngle())), posright, true);
+
+		// Render the world
+		renderer.render(scene.getWorld(), cam.combined);
+
+		// Draw the forces(the world renderer will not display individual forces
+		// :( ).
+		cam.update();
+		shape.setProjectionMatrix(cam.combined);
+		shape.begin(ShapeType.Line);
+		shape.setColor(Color.YELLOW);
+		shape.circle(posleft.x, posleft.y, 0.01f, 10);
+		shape.line(
+				posleft,
+				new Vector2(posleft.x + forces.x
+						* MathUtils.cos(1.5f * MathUtils.PI + quad.getAngle())
+						/ ppm, posleft.y + forces.x
+						* MathUtils.sin(1.5f * MathUtils.PI + quad.getAngle())
+						/ ppm));
+
+		shape.line(
+				posright,
+				new Vector2(posright.x + forces.y
+						* MathUtils.cos(1.5f * MathUtils.PI + quad.getAngle())
+						/ ppm, posright.y + forces.y
+						* MathUtils.sin(1.5f * MathUtils.PI + quad.getAngle())
+						/ ppm));
+
+		shape.circle(posright.x, posright.y, 0.01f, 10);
+		
+		shape.end();
 
 		scene.step();
 
-		renderer.render(scene.getWorld(), cam.combined);
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		cam.setToOrtho(false, width/ppm, height/ppm);
+	}
+	
+	@Override
+	public void dispose() {
+		shape.dispose();
+		scene.getWorld().dispose();
+		renderer.dispose();
+	}
 
+	//////////////////////////////////////////////////////////
+	//
+	//	Input Processor
+	//
+	//////////////////////////////////////////////////////////
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Keys.R) {
+			init();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		cam.zoom = MathUtils.clamp(cam.zoom + amount*0.1f, 0.1f, 10);
+		
+		return false;
 	}
 }
--- sim/core/src/com/ttocsneb/quadsim/QuadAI.java
+++ sim/core/src/com/ttocsneb/quadsim/QuadAI.java
@@ -1,6 +1,7 @@
 package com.ttocsneb.quadsim;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.math.Vector2;
 
 public class QuadAI {
@@ -9,11 +10,18 @@ public class QuadAI {
 		
 	}
 	
+	/**
+	 * Update the drone flight controller.
+	 * @param gyroData angular velocity
+	 * @param accelData deviation from free fall (relative to the craft.).
+	 * @return (Left, Right) power outputs (0-1000)
+	 */
 	public Vector2 update(float gyroData, Vector2 accelData) {
 		
 		Gdx.app.debug("Data", "G " + gyroData + " X " + accelData.x + " Y " + accelData.y);
 		
-		return null;
+		
+		return new Vector2(Gdx.input.isKeyPressed(Keys.LEFT) ? 250 : 0, Gdx.input.isKeyPressed(Keys.RIGHT) ? 250 : 0);
 	}
 	
 }
